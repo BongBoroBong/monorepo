a5c7b366a6ce849fec2638fa7af1a1e9251897be
김봉우 <bangducks@naver.com>
2022-04-07 17:56:46 +0900

    :tada: initailize

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { getLoader, loaderByName } = require('@craco/craco');
+const CracoAlias = require('craco-alias');
 const CracoItkPlugin = require('craco-itk');
 
 const vtkPath = path.join(__dirname, '../vtk-module');
@@ -7,18 +8,64 @@ const componentPath = path.join(__dirname, '../component-module');
 
 module.exports = {
   webpack: {
+    mode: 'production',
+    optimization: {
+      optimization: {
+        minimize: true,
+        minimizer: [
+          (compiler) => {
+            const TerserPlugin = require('terser-webpack-plugin');
+            new TerserPlugin({
+              terserOptions: {
+                compress: {},
+              },
+            }).apply(compiler);
+          },
+        ],
+      },
+    },
     configure: (webpackConfig) => {
       const { isFound, match } = getLoader(webpackConfig, loaderByName('babel-loader'));
       if (isFound) {
         const include = Array.isArray(match.loader.include)
-            ? match.loader.include
-            : [match.loader.include];
-        match.loader.include = include.concat[vtkPath, componentPath];
+          ? match.loader.include
+          : [match.loader.include];
+        match.loader.include = include.concat[(vtkPath, componentPath)];
       }
       return webpackConfig;
     },
   },
-  plugins:[
-    {plugin:CracoItkPlugin()}
-  ]
+  babel: {
+    presets: [
+      ['@babel/preset-env', { loose: true }],
+      ['@babel/preset-react', { absoluteRuntime: false }],
+    ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+        },
+      ],
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          legacy: true,
+        },
+      ],
+    ],
+  },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './',
+        tsConfigPath: './tsconfig.json',
+      },
+    },
+    {
+      plugin: CracoItkPlugin(),
+    },
+  ],
 };
